534306a5a97193761f8ab2d7191bfbd27bcccd6b
lky473736 <lky473736@icloud.com>
2024-04-10 21:13:47 +0900

    feat : 드롭다운으로 선택 정렬 기능 구현

--- routes/search.js
+++ routes/search.js
@@ -16,9 +16,23 @@ connectDB.then((client)=>{
 router.get('/:page', async (요청, 응답) => {
     // 현재 페이지에 해당되는 5개 이하의 게시물을 postlist에 저장
     console.log (요청.query.keyword)
+    let option = 요청.query.sort;
+    let findByOption;
+
+    if (option === "title") {
+      findByOption = {"title" : {'$regex' : 요청.query.keyword}};
+    }
+    else if (option === "content") {
+      findByOption = {"content" : {'$regex' : 요청.query.keyword}};
+    }
+    else {
+      findByOption = {"name" : {'$regex' : 요청.query.keyword}};
+    }
+    console.log (findByOption);
+
     try {
-        let postlist = await db.collection('post').find({"title" : {'$regex' : 요청.query.keyword}}).skip((요청.params.page-1) * 5).limit(5).toArray();
-        let allPostlist = await db.collection('post').find({"title" : {'$regex' : 요청.query.keyword}}).toArray();
+        let postlist = await db.collection('post').find(findByOption).skip((요청.params.page-1) * 5).limit(5).toArray();
+        let allPostlist = await db.collection('post').find(findByOption).toArray();
     
         응답.render('result.ejs', {글목록 : postlist, 페이지 : 요청.params.page, 글수 : allPostlist.length, 로그인상태 : 요청.user, 검색키워드 : 요청.query.keyword});
         console.log ("keyword를 search -> list up한 게시물을 보여줌 / 게시물 수 : ", allPostlist.length)
