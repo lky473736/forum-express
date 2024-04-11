f22f5b8e3b4dd87ba74d2865413c2e755d4f4b6b
lky473736 <lky473736@icloud.com>
2024-04-11 17:57:27 +0900

    feat : 마이페이지에서 자신이 작성한 글 볼 수 있도록 기능 구현

--- routes/mypage.js
+++ routes/mypage.js
@@ -14,9 +14,12 @@ connectDB.then((client)=>{
   console.log(err)
 }) 
 
-router.get ("/", checkLogin, async(요청, 응답) => {
+router.get ("/:page", checkLogin, async(요청, 응답) => {
     console.log('마이페이지 들어감, 현재 로그인 상태 : ', 요청.user);
-    응답.render("mypage.ejs", {로그인상태 : 요청.user});
+    let postlist = await db.collection('post').find({"user" : 요청.user}).skip((요청.params.page-1) * 5).limit(5).toArray();
+    let allPostlist = await db.collection('post').find().toArray();
+
+    응답.render("mypage.ejs", {로그인상태 : 요청.user, 작성글 : allPostlist, 글수 : allPostlist.length, 페이지 : 요청.params.page});
 });
 
 module.exports = router 
